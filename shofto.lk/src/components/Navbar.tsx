bac314f330a5a13d1f789996145af026cc005546
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-01-31 01:34:35 +0530

    Navbar Initiated

---
+++ shofto.lk/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <div className="hidden lg:block">
+        <div className="container">
+            <div className="flex w-fit gap-10 mx-auto font-medium py-4 text-blackish">
+                <Link className="navbar__link relative" href="#">
+                    Home
+                
+                </Link>
+                
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
